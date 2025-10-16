e32b4f26a3bc7771efc118951a22ab33e4037911
Pedro Calado <pedroocalado@gmail.com>
2025-10-16 10:46:32 +0000

    Enforcing security and good pratices

--- LOFERSIL-Web_store/utils/auth.ts
+++ LOFERSIL-Web_store/utils/auth.ts
@@ -10,3 +10,18 @@ export async function verifyPassword(
 ): Promise<boolean> {
   return await compare(password, hash);
 }
+
+// Add to utils/auth.ts
+export function validatePassword(password: string): boolean {
+  const minLength = 8;
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasLowerCase = /[a-z]/.test(password);
+  const hasNumbers = /\d/.test(password);
+  const hasSpecialChar = /[!@#$%^&*]/.test(password);
+  
+  return password.length >= minLength && 
+         hasUpperCase && 
+         hasLowerCase && 
+         hasNumbers && 
+         hasSpecialChar;
+}
---
+++ src/utils/auth.ts
@@ -0,0 +1,9 @@
+import * as bcrypt from "https://deno.land/x/bcrypt@v0.4.1/mod.ts";
+
+export async function hashPassword(password: string): Promise<string> {
+  return await bcrypt.hash(password);
+}
+
+export async function verifyPassword(password: string, hash: string): Promise<boolean> {
+  return await bcrypt.compare(password, hash);
+}
